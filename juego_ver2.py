ad697b597caf1e7127937aee916b175d9f40a97d
Hemiptera1 <55675342+Hemiptera1@users.noreply.github.com>
2019-10-03 23:58:35 -0300

    progreso

---
+++ juego_ver2.py
@@ -0,0 +1,15 @@
+# Crear clase Personaje
+# Crear instancia Caballero
+# Crear instancia Orco
+# Crear clase Choza
+
+class Personaje():
+
+	def __init__(self, nombre, vida, tipo):
+		self.nombre = nombre
+		self.vida = 0
+		self.tipo = None
+
+class Caballero(Personaje):
+
+	def __init__(self, )
